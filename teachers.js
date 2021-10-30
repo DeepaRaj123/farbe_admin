90e00c64285ab378e49373e5b4bebf4983e5239f
DeepaRaj123 <36413136+DeepaRaj123@users.noreply.github.com>
2021-10-30 13:10:41 +0530

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,53 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("button1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  document.getElementById("registration-password").value=""
+  document.getElementById("registration-email").value=""
+  document.getElementById("registration-name").value=""
+  document.getElementById("registration-number").value=""
+  document.getElementById("registration-course").value=""
+  document.getElementById("registration-reemail").value=""
+  document.getElementById("registration-job").value=""
+  document.getElementById("registration-parentcontacct").value=""
+  document.getElementById("registration-parent").value=""
+  document.getElementById("registration-teacher").value=""
+  document.getElementById("registration-classes").value=""
+document.getElementById("registration-email").value=""
+document.getElementById("link1").innerText=""
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById("registration-password").value=""
+    document.getElementById("registration-email").value=""
+    document.getElementById("registration-name").value=""
+    document.getElementById("registration-number").value=""
+    document.getElementById("registration-course").value=""
+    document.getElementById("registration-reemail").value=""
+    document.getElementById("registration-job").value=""
+    document.getElementById("registration-parentcontacct").value=""
+    document.getElementById("registration-parent").value=""
+    document.getElementById("registration-teacher").value=""
+    document.getElementById("registration-classes").value=""
+ document.getElementById("registration-email").value=""
+
+  }
+}
